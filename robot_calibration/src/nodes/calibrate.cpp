c51243d4d77473dc8f97568dc6498258a741213a
Austin Gregg-Smith <blooop@gmail.com>
2025-08-19 14:53:00 +0000

    increase sleep timeout for settling

--- robot_calibration/src/nodes/calibrate.cpp
+++ robot_calibration/src/nodes/calibrate.cpp
@@ -163,7 +163,7 @@ int main(int argc, char** argv)
         }
 
         // Make sure sensor data is up to date after settling
-        rclcpp::sleep_for(std::chrono::milliseconds(100));
+        rclcpp::sleep_for(std::chrono::milliseconds(200));
 
         // Get pose of the features
         if (!capture_manager.captureFeatures(poses[pose_idx].features, msg))
