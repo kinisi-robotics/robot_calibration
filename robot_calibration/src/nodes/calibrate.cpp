a91461b68afbf429e71b9c09b30ca87ab5da8326
Austin Gregg-Smith <blooop@gmail.com>
2025-09-18 13:07:20 +0000

    turn off verbose loggin

--- robot_calibration/src/nodes/calibrate.cpp
+++ robot_calibration/src/nodes/calibrate.cpp
@@ -214,7 +214,7 @@ int main(int argc, char** argv)
   for (auto step : calibration_steps)
   {
     params.LoadFromROS(node, step);
-    opt.optimize(params, data, logger, verbose);
+    opt.optimize(params, data, logger, false);
     if (verbose)
     {
       std::cout << "Parameter Offsets:" << std::endl;
